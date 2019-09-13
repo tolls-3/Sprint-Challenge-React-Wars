a900f9a8b534cf034d6250db008b3810dd487c22
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-13 09:08:14 +0100

    created peoplelist file to map over data

---
+++ starwars/src/components/People.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function People (props){
+// const People = props => {
+
+const { name, height, mass, birth_year, gender,skin_color } = props;
+  return (
+    <div >
+      <h2><span>Name:</span> {name}</h2>
+      <p><span>Height:</span> {height}</p>
+      <p><span>Mass:</span> {mass}</p>
+      <p><span>Birth Year:</span> {birth_year}</p>
+      <p><span>Gender:</span> {gender}</p>
+      <p><span>Skin Color:</span> {skin_color}</p>
+    </div>
+  );
+};
+
+export default People;
---
+++ starwars/src/components/Peoplelist.js
@@ -0,0 +1,18 @@
+import React from "react";
+import People from "./People";
+
+
+
+function Peoplelist (props){
+// const Peoplelist = props => {
+    
+  return (
+    <div>
+      
+      {props.data.map(character => (
+        <People key={character} data={character} />
+      ))}
+    </div>
+
+  );
+};
